e4608fee5e6a2e534ebfcdf8e4ee68eaa5617259
Yogendra Singh <yogendrayadav168@gmail.com>
2025-10-26 23:11:21 +0530

    updated backend url in forntend

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { API_URL } from '../constant/apiUrl';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: `${API_URL}/api`,
   headers: {
     'Content-Type': 'application/json',
     
