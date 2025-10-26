cbe3ebec8c86a8a6fea9554ce981413adb317a5d
Yogendra Singh <yogendrayadav168@gmail.com>
2025-10-26 23:14:44 +0530

    updated cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -37,6 +37,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
+      "https://task-manager-ivory-seven.vercel.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
     allowedHeaders: [
